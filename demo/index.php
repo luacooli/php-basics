8247962ca673513bdd1a432690d87be3a9cb76e4
Luana <lcorreia@vntrx.com>
2023-06-21 15:18:41 -0300

    lesson: conditions and booleans

---
+++ demo/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        $name = "Dark Matter";
+        $read = false;
+
+        if ($read) {
+            $message = "You have read $name";
+        } else {
+            $message = "You have NOT read $name";
+        }
+    ?>
+
+    <h1>
+        <!-- <?php echo $message; ?> -->
+        <?= $message ?>
+    </h1>
+</body>
+</html>
